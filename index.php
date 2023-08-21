95dc5507c66156beb20a637f34731b527a1ffac8
Angel Kasabov <ankasabov142@gmail.com>
2023-08-21 16:47:39 +0300

    New pdates

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo "I like pizza";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login-Register Page via PHP</title>
+</head>
+<body>
+    
+</body>
+</html>

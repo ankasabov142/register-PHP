1a8b393f40efe59fe4024ade4c20a2f0c4a192d5
Angel Kasabov <ankasabov142@gmail.com>
2023-08-22 17:28:02 +0300

    Updated form

--- dataBaseConnection.php
+++ dataBaseConnection.php
@@ -7,12 +7,12 @@ $conn = "";
 
 try{
 $conn = mysqli_connect($db_server,
-$db_user,
-$db_pass,
-$db_name);
+                       $db_user,
+                       $db_pass,
+                       $db_name);
 }
 catch(mysqli_sql_exception){
-    echo "Could not connect! <br>"; 
+    echo "Could not connect to the data base! <br>"; 
 }
 
 ?>
--- index.php
+++ index.php
@@ -10,6 +10,28 @@ echo "I like sspzza";
     <title>Login-Register Page via PHP</title>
 </head>
 <body>
-    <h1>Hello</h1>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+<h1>Welcome!</h1>
+<p>Username:</p>
+<input type="text" name="username">
+
+<p>Email:</p>
+<input type="email" name="email">
+
+<p>Password:</p>
+<input type="password" name="password">
+
+<p>Phone Number:</p>
+<input type="text" name="phone">
+<br>
+<br>
+
+<input type="submit" name="submit" value="Register now">
+</form>
 </body>
 </html>
+
+
+<?php 
+mysqli_close($conn);
+?>

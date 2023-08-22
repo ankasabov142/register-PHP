80d4ae66ac2e1bdcfedda432c195bc5f217f5220
Angel Kasabov <ankasabov142@gmail.com>
2023-08-22 19:34:31 +0300

    Updated login page and created profile page

--- loginPage.php
+++ loginPage.php
@@ -1,5 +1,6 @@
 <?php 
 include"dataBaseConnection.php";
+session_start();
 ?>
 
 <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
@@ -29,46 +30,18 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
             $row =mysqli_fetch_assoc($result);
              $hashPass =$row['password'];
              if(password_verify($password,$hashPass)){
-                echo 'Sucssefully log in';
+                $_SESSION['id']=$row['id'];
+                echo "<p>Sucssefully log in!</p> <br>
+    <a href='profilePage.php'>Proceed to Profile page</a>";
              } else{
                 throw new Exception();
             }
     }
 }
     catch (Exception ) {
-        echo "Wrong shit bro";
+        echo "Wrong password or username";
     }
 
 }
-// if($_SERVER["REQUEST_METHOD"]=="POST"){
-//     $username = filter_input(INPUT_POST,"username", FILTER_SANITIZE_SPECIAL_CHARS);
-//     $email = filter_input(INPUT_POST,"email", FILTER_SANITIZE_EMAIL);
-//     $password = filter_input(INPUT_POST,"password", FILTER_SANITIZE_SPECIAL_CHARS);
-//     $phone = preg_replace('/[^0-9]/', '', $_POST['phone']);
-
-//     if(empty($username)|| empty($email)|| empty($password)|| empty($phone)){
-//         echo('<p>Please fill in all required fields </p>');
-//         echo('<p>P.S. Your phone number must contain only 10 digits</p>');
-//     }
-//     else{
-//         $hash = password_hash($password,PASSWORD_DEFAULT);
-//       $sql = "INSERT INTO registeredusers (username,email,password,phone)
-//     VALUES('$username','$email','$hash','$phone')";
-// try{
-//     mysqli_query($conn,$sql);
-//     echo "<p>You are now registered!</p> <br>
-//     <a href='loginPage.php'>Proceed to Login page</a>";
-// }       
-// catch(mysqli_sql_exception){
-//     echo "<p>There was an error filling in your details </p>";
-// echo "<p>Or they are already used </p>";
-// }
-       
-
-
-//     }
-// }
-
-/*if (filter_var($email, FILTER_VALIDATE_EMAIL))*/
 mysqli_close($conn);
 ?>
